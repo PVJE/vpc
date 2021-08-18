053add615a72079fc8e44fffdb7d635859190abd
Volodymyr Pryshutov <Aleksandr.Zhuravel@alfabank.kiev.ua>
2021-08-18 18:19:59 +0300

    tags

--- vpc.go
+++ vpc.go
@@ -17,9 +17,9 @@ type Config struct {
 	// }
 
 	Vpc []struct {
-		Name         string `yaml:"Name"`
+		Name         string `yaml:"name"`
 		Cidr_block   string `yaml:"Cidr_block"`
-		DnsHostnames bool   `yaml:"DnsHostnames"`
+		DnsHostnames bool   `yaml:"dnsHostnames"`
 	}
 }
 
