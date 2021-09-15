32527f6377b3553c76062f163dd9fb084c2c6f23
Volodymyr Pryshutov <Aleksandr.Zhuravel@alfabank.kiev.ua>
2021-09-15 15:56:55 +0300

    rework tags

--- vpc.go
+++ vpc.go
@@ -5,76 +5,26 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-// type Vpc struct {
-// 	// Name         string
-// 	// Cidr_block   string
-// 	// DnsHostnames bool
-// 	Name         interface{}
-// 	Cidr_block   interface{}
-// 	DnsHostnames interface{}
-// }
-
-// type Output struct {
-// 	out *ec2.Vpc
-// }
-
-func CreateVPC(vpc_name string, vpc_cidr string, vpc_dnshostanme bool, tags map[string]string, ctx *pulumi.Context) (*ec2.Vpc, error) {
-	//pulumi.Run(func(ctx *pulumi.Context) error {
-	//id := make([]string, len(Vpc))
-	// name := vpc.Name.(string)
-	// cidr_block := vpc.Cidr_block.(string)
-	// dnsHostnames := vpc.Name.(bool)
-	myvpc, err := ec2.NewVpc(ctx, string(vpc_name), &ec2.VpcArgs{
-		AssignGeneratedIpv6CidrBlock: pulumi.Bool(false),
-		CidrBlock:                    pulumi.String(string(vpc_cidr)),
-		//CidrBlock:          pulumi.String(string("10.9.48.64/27")),
-		EnableDnsSupport:   pulumi.Bool(true),
-		EnableDnsHostnames: pulumi.Bool(bool(vpc_dnshostanme)),
-		InstanceTenancy:    pulumi.String("default"),
-		Tags: pulumi.StringMap{
-			"Name":        pulumi.String(string(vpc_name)),
-			"Project":     pulumi.String(string(tags["Project"])),
-			"Project-env": pulumi.String(string(tags["Project_env"])),
-			// "Name": pulumi.String(string(Vpc[i].Name)),
-			// "Project":     pulumi.String(string(Tags.Project)),
-			// "Project-env": pulumi.String(string(Tags.Project_env)),
-		},
-	}, pulumi.Protect(false))
-	if err != nil {
-		return nil, err
+func CreateVPC(vpc_name string, vpc_cidr string, vpc_dnshostanme bool, tags_key []string, tags_values []string, ctx *pulumi.Context) (*ec2.Vpc, error) {
+	var my_vpc *ec2.Vpc
+	var err error
+	for i := range tags_key {
+		my_vpc, err = ec2.NewVpc(ctx, string(vpc_name), &ec2.VpcArgs{
+			AssignGeneratedIpv6CidrBlock: pulumi.Bool(false),
+			CidrBlock:                    pulumi.String(string(vpc_cidr)),
+			EnableDnsSupport:             pulumi.Bool(true),
+			EnableDnsHostnames:           pulumi.Bool(bool(vpc_dnshostanme)),
+			InstanceTenancy:              pulumi.String("default"),
+			Tags: pulumi.StringMap{
+				tags_key[i]: pulumi.String(string(tags_values[i])),
+				"pulumi":    pulumi.String(string(vpc_name)),
+				// "Project":     pulumi.String(string(tags["Project"])),
+				// "Project-env": pulumi.String(string(tags["Project_env"])),
+			},
+		}, pulumi.Protect(false))
+		if err != nil {
+			return nil, err
+		}
 	}
-	//ctx.Export("VPC_id", id.ID())
-
-	// 	return nil
-	// })
-
-	// pulumi.Run(func(ctx *pulumi.Context) {
-	// 	var cfg Config
-	// 	// var id [len(Vpc)]string
-	// 	readFile(&cfg, file_name)
-	// 	// id := make([]string, len(Vpc))
-	// 	for i := range Vpc {
-	// 		id, err := ec2.NewVpc(ctx, string("My_VPC_"+strconv.Itoa(i)), &ec2.VpcArgs{
-	// 			AssignGeneratedIpv6CidrBlock: pulumi.Bool(false),
-	// 			CidrBlock:                    pulumi.String(string(Vpc[0].Cidr_block)),
-	// 			//CidrBlock:          pulumi.String(string("10.9.48.64/27")),
-	// 			EnableDnsSupport:   pulumi.Bool(true),
-	// 			EnableDnsHostnames: pulumi.Bool(bool(Vpc[i].DnsHostnames)),
-	// 			InstanceTenancy:    pulumi.String("default"),
-	// 			Tags: pulumi.StringMap{
-	// 				"Name":        pulumi.String(string(Vpc[i].Name)),
-	// 				"Project":     pulumi.String(string(Tags.Project)),
-	// 				"Project-env": pulumi.String(string(Tags.Project_env)),
-	// 			},
-	// 		}, pulumi.Protect(false))
-	// 		if err != nil {
-	// 			return err
-	// 		}
-
-	// 	}
-	// output := new(Output)
-	// output.out = myvpc
-	return myvpc, nil
-	// })
-
+	return my_vpc, nil
 }
