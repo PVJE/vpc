a017ae2525fa972bade6d77fbef3c9aa369fe25f
Volodymyr Pryshutov <Aleksandr.Zhuravel@alfabank.kiev.ua>
2021-09-16 15:12:21 +0300

    fix arg in func

--- vpc.go
+++ vpc.go
@@ -6,12 +6,12 @@ import (
 	ph "github.com/pvje/pulumihelper"
 )
 
-func CreateVPC(vpc_name string, vpc_assign_generated_ipv6_cidr_block bool, vpc_cidr string, vpc_enable_dns_support bool, vpc_dns_hostanme bool, vpc_instance_tenancy string, tags map[string]string, ctx *pulumi.Context) (*ec2.Vpc, error) {
+func CreateVPC(vpc_name string, vpc_assign_generated_ipv6_cidr_block bool, vpc_cidr string, vpc_enable_dns_support bool, vpc_dns_hostanme bool, vpc_instance_tenancy string, tags_keys []string, tags_values []string, ctx *pulumi.Context) (*ec2.Vpc, error) {
 	// tagsMap := make(pulumi.StringMap)
 	// for k := range tags_key {
 	// 	tagsMap[tags_key[k]] = pulumi.String(tags_values[k])
 	// }
-	tagsMap := ph.ToPulumiStringMap(tags)
+	tagsMap := ph.ToPulumiStringMap(tags_keys, tags_values)
 	my_vpc, err := ec2.NewVpc(ctx, string(vpc_name), &ec2.VpcArgs{
 		AssignGeneratedIpv6CidrBlock: pulumi.Bool(bool(vpc_assign_generated_ipv6_cidr_block)),
 		CidrBlock:                    pulumi.String(string(vpc_cidr)),
