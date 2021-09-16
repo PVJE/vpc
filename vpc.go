a4f1cb95b866fb1adb75bc7f57cc9854ab2e76f8
Volodymyr Pryshutov <Aleksandr.Zhuravel@alfabank.kiev.ua>
2021-09-16 08:36:51 +0300

    refactor func args

--- vpc.go
+++ vpc.go
@@ -5,17 +5,17 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-func CreateVPC(vpc_name string, vpc_cidr string, vpc_dnshostanme bool, tags_key []string, tags_values []string, ctx *pulumi.Context) (*ec2.Vpc, error) {
+func CreateVPC(vpc_name string, vpc_assign_generated_ipv6_cidr_block bool, vpc_cidr string, vpc_enable_dns_support bool, vpc_dns_hostanme bool, vpc_instance_tenancy string, tags_key []string, tags_values []string, ctx *pulumi.Context) (*ec2.Vpc, error) {
 	tagsMap := make(pulumi.StringMap)
 	for k := range tags_key {
 		tagsMap[tags_key[k]] = pulumi.String(tags_values[k])
 	}
 	my_vpc, err := ec2.NewVpc(ctx, string(vpc_name), &ec2.VpcArgs{
-		AssignGeneratedIpv6CidrBlock: pulumi.Bool(false),
+		AssignGeneratedIpv6CidrBlock: pulumi.Bool(bool(vpc_assign_generated_ipv6_cidr_block)),
 		CidrBlock:                    pulumi.String(string(vpc_cidr)),
-		EnableDnsSupport:             pulumi.Bool(true),
-		EnableDnsHostnames:           pulumi.Bool(bool(vpc_dnshostanme)),
-		InstanceTenancy:              pulumi.String("default"),
+		EnableDnsSupport:             pulumi.Bool(bool(vpc_enable_dns_support)),
+		EnableDnsHostnames:           pulumi.Bool(bool(vpc_dns_hostanme)),
+		InstanceTenancy:              pulumi.String(string(vpc_instance_tenancy)),
 		Tags:                         pulumi.StringMap(tagsMap),
 	}, pulumi.Protect(false))
 	if err != nil {
